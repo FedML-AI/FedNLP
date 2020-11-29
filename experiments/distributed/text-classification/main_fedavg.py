57d5f30f02ad801a80a59b18d983125f1c0bfcbe
chaoyanghe <me@chaoyanghe.com>
2020-11-29 14:04:56 -0800

    support customized model trainer

--- experiments/distributed/text-classification/main_fedavg.py
+++ experiments/distributed/text-classification/main_fedavg.py
@@ -15,9 +15,73 @@ import wandb
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../../FedML")))
 from FedML.fedml_api.distributed.fedavg.FedAvgAPI import FedML_init, FedML_FedAvg_distributed
+from FedML.fedml_core.trainer.model_trainer import ModelTrainer
 
 from data_preprocessing.shakespeare.data_loader import load_partition_data_shakespeare
 from model.rnn import RNN_OriginalFedAvg
+
+
+class NLPModelTrainer(ModelTrainer):
+    def get_model_params(self):
+        return self.model.cpu().state_dict()
+
+    def set_model_params(self, model_parameters):
+        self.model.load_state_dict(model_parameters)
+
+    def train(self, train_data, device, args):
+        model = self.model
+
+        model.to(device)
+        model.train()
+
+        criterion = nn.CrossEntropyLoss().to(device)
+        if args.client_optimizer == "sgd":
+            optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+        else:
+            optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
+                                         lr=args.lr,
+                                         weight_decay=args.wd, amsgrad=True)
+        epoch_loss = []
+        for epoch in range(args.epochs):
+            batch_loss = []
+            for batch_idx, (x, labels) in enumerate(train_data):
+                # logging.info(images.shape)
+                x, labels = x.to(device), labels.to(device)
+                optimizer.zero_grad()
+                log_probs = model(x)
+                loss = criterion(log_probs, labels)
+                loss.backward()
+                optimizer.step()
+                batch_loss.append(loss.item())
+            if len(batch_loss) > 0:
+                epoch_loss.append(sum(batch_loss) / len(batch_loss))
+                logging.info('(Trainer_ID {}. Local Training Epoch: {} \tLoss: {:.6f}'.format(self.id,
+                                                                                              epoch,
+                                                                                              sum(epoch_loss) / len(
+                                                                                                  epoch_loss)))
+
+    def test(self, test_data, device, args):
+        model = self.model
+
+        model.eval()
+        model.to(device)
+
+        test_loss = test_acc = test_total = 0.
+        criterion = nn.CrossEntropyLoss().to(device)
+        with torch.no_grad():
+            for batch_idx, (x, target) in enumerate(test_data):
+                x = x.to(device)
+                target = target.to(device)
+                pred = model(x)
+                loss = criterion(pred, target)
+                _, predicted = torch.max(pred, -1)
+                correct = predicted.eq(target).sum()
+
+                test_acc += correct.item()
+                test_loss += loss.item() * target.size(0)
+                test_total += target.size(0)
+
+        return test_acc, test_total, test_loss
 
 
 def add_args(parser):
@@ -144,7 +208,8 @@ if __name__ == "__main__":
     # customize the log format
     logging.getLogger().setLevel(logging.INFO)
     logging.basicConfig(level=logging.INFO,
-                        format=str(process_id) + ' - %(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        format=str(
+                            process_id) + ' - %(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                         datefmt='%a, %d %b %Y %H:%M:%S')
     hostname = socket.gethostname()
     logging.info("#############process ID = " + str(process_id) +
@@ -195,7 +260,11 @@ if __name__ == "__main__":
     # In this case, please use our FedML distributed version (./fedml_experiments/distributed_fedavg)
     model = create_model(args, model_name=args.model, output_dim=dataset[7])
 
+    # define the customized model trainer using abstract class `FedML.fedml_core.trainer.model_trainer`
+    model_trainer = NLPModelTrainer(model)
+
     # start "federated averaging (FedAvg)"
     FedML_FedAvg_distributed(process_id, worker_number, device, comm,
                              model, train_data_num, train_data_global, test_data_global,
-                             train_data_local_num_dict, train_data_local_dict, test_data_local_dict, args)
+                             train_data_local_num_dict, train_data_local_dict, test_data_local_dict, args,
+                             model_trainer)
