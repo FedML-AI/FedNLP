64549bc826833a158b35e23c2f716272d0063c08
chaoyanghe <me@chaoyanghe.com>
2021-02-17 11:37:13 -0800

    show an example of simplified transformer training

---
+++ model/transformer/model_args.py
@@ -0,0 +1,132 @@
+import json
+import os
+import sys
+from dataclasses import asdict, dataclass, field
+from multiprocessing import cpu_count
+
+
+def get_default_process_count():
+    process_count = cpu_count() - 2 if cpu_count() > 2 else 1
+    if sys.platform == "win32":
+        process_count = min(process_count, 61)
+
+    return process_count
+
+
+def get_special_tokens():
+    return ["<s>", "<pad>", "</s>", "<unk>", "<mask>"]
+
+
+@dataclass
+class ModelArgs:
+    adam_epsilon: float = 1e-8
+    best_model_dir: str = "outputs/best_model"
+    cache_dir: str = "cache_dir/"
+    config: dict = field(default_factory=dict)
+    custom_layer_parameters: list = field(default_factory=list)
+    custom_parameter_groups: list = field(default_factory=list)
+    dataloader_num_workers: int = field(default_factory=get_default_process_count)
+    do_lower_case: bool = False
+    dynamic_quantize: bool = False
+    early_stopping_consider_epochs: bool = False
+    early_stopping_delta: float = 0
+    early_stopping_metric: str = "eval_loss"
+    early_stopping_metric_minimize: bool = True
+    early_stopping_patience: int = 3
+    encoding: str = None
+    eval_batch_size: int = 8
+    evaluate_during_training: bool = False
+    evaluate_during_training_silent: bool = True
+    evaluate_during_training_steps: int = 2000
+    evaluate_during_training_verbose: bool = False
+    evaluate_each_epoch: bool = True
+    fp16: bool = True
+    gradient_accumulation_steps: int = 1
+    learning_rate: float = 4e-5
+    local_rank: int = -1
+    logging_steps: int = 50
+    manual_seed: int = None
+    max_grad_norm: float = 1.0
+    max_seq_length: int = 128
+    model_name: str = None
+    model_type: str = None
+    multiprocessing_chunksize: int = 500
+    n_gpu: int = 1
+    no_cache: bool = False
+    no_save: bool = False
+    not_saved_args: list = field(default_factory=list)
+    num_train_epochs: int = 1
+    output_dir: str = "outputs/"
+    overwrite_output_dir: bool = False
+    process_count: int = field(default_factory=get_default_process_count)
+    quantized_model: bool = False
+    reprocess_input_data: bool = True
+    save_best_model: bool = True
+    save_eval_checkpoints: bool = True
+    save_model_every_epoch: bool = True
+    save_optimizer_and_scheduler: bool = True
+    save_steps: int = 2000
+    silent: bool = False
+    tensorboard_dir: str = None
+    thread_count: int = None
+    train_batch_size: int = 8
+    train_custom_parameters_only: bool = False
+    use_cached_eval_features: bool = False
+    use_early_stopping: bool = False
+    use_multiprocessing: bool = True
+    wandb_kwargs: dict = field(default_factory=dict)
+    wandb_project: str = None
+    warmup_ratio: float = 0.06
+    warmup_steps: int = 0
+    weight_decay: int = 0
+    skip_special_tokens: bool = True
+
+    def update_from_dict(self, new_values):
+        if isinstance(new_values, dict):
+            for key, value in new_values.items():
+                setattr(self, key, value)
+        else:
+            raise (TypeError(f"{new_values} is not a Python dict."))
+
+    def get_args_for_saving(self):
+        args_for_saving = {key: value for key, value in asdict(self).items() if key not in self.not_saved_args}
+        return args_for_saving
+
+    def save(self, output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+        with open(os.path.join(output_dir, "model_args.json"), "w") as f:
+            json.dump(self.get_args_for_saving(), f)
+
+    def load(self, input_dir):
+        if input_dir:
+            model_args_file = os.path.join(input_dir, "model_args.json")
+            if os.path.isfile(model_args_file):
+                with open(model_args_file, "r") as f:
+                    model_args = json.load(f)
+
+                self.update_from_dict(model_args)
+
+
+@dataclass
+class ClassificationArgs(ModelArgs):
+    """
+    Model args for a ClassificationModel
+    """
+
+    model_class: str = "ClassificationModel"
+    labels_list: list = field(default_factory=list)
+    labels_map: dict = field(default_factory=dict)
+    lazy_delimiter: str = "\t"
+    lazy_labels_column: int = 1
+    lazy_loading: bool = False
+    lazy_loading_start_line: int = 1
+    lazy_text_a_column: bool = None
+    lazy_text_b_column: bool = None
+    lazy_text_column: int = 0
+    onnx: bool = False
+    regression: bool = False
+    sliding_window: bool = False
+    stride: float = 0.8
+    tie_value: int = 1
+    evaluate_during_training_steps: int = 20
+    evaluate_during_training: bool = True
---
+++ training/tc_transformer_trainer.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import absolute_import, division, print_function
+
+import logging
+import math
+import os
+
+import numpy as np
+import sklearn
+import torch
+import wandb
+from sklearn.metrics import (
+    confusion_matrix,
+    matthews_corrcoef,
+)
+from torch.nn import CrossEntropyLoss
+
+from transformers import (
+    AdamW,
+    get_linear_schedule_with_warmup,
+)
+
+
+class TextClassificationTrainer:
+    def __init__(self, args, device, model, train_dl, test_dl, test_examples):
+        self.args = args
+        self.device = device
+
+        # training data
+        self.num_labels = args.num_labels
+        self.test_examples = test_examples
+        self.train_dl = train_dl
+        self.test_dl = test_dl
+
+        # model
+        self.model = model
+
+        # training results
+        self.results = {}
+        self.best_accuracy = 0.0
+
+    def train_model(self):
+
+        # build optimizer and scheduler
+        iteration_in_total = len(
+            self.train_dl) // self.args.gradient_accumulation_steps * self.args.num_train_epochs
+        optimizer, scheduler = self.build_optimizer(self.model, iteration_in_total)
+
+        # training result
+        global_step = 0
+        tr_loss, logging_loss = 0.0, 0.0
+        for epoch in range(0, self.args.num_train_epochs):
+
+            self.model.train()
+
+            for batch_idx, batch in enumerate(self.train_dl):
+
+                batch = tuple(t for t in batch)
+                # inputs = {"input_ids": batch[0], "attention_mask": batch[1], "labels": batch[3]}
+                x = batch[1].to(self.device)
+                labels = batch[4].to(self.device)
+
+                logits = self.model(x)
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+                # model outputs are always tuple in pytorch-transformers (see doc)
+                # loss = outputs[0]
+                # logging.info(loss)
+                current_loss = loss.item()
+                logging.info("epoch = %d, batch_idx = %d/%d, loss = %s" % (epoch, batch_idx,
+                                                                           len(self.train_dl), current_loss))
+
+                if self.args.gradient_accumulation_steps > 1:
+                    loss = loss / self.args.gradient_accumulation_steps
+
+                loss.backward()
+
+                tr_loss += loss.item()
+                if (batch_idx + 1) % self.args.gradient_accumulation_steps == 0:
+                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.args.max_grad_norm)
+                    optimizer.step()
+                    scheduler.step()  # Update learning rate schedule
+                    self.model.zero_grad()
+                    global_step += 1
+
+                    if self.args.evaluate_during_training and (self.args.evaluate_during_training_steps > 0
+                                                               and global_step % self.args.evaluate_during_training_steps == 0):
+                        results, _, _ = self.eval_model(epoch, global_step)
+                        logging.info(results)
+
+                if self.args.is_debug_mode == 1 and global_step > 3:
+                    break
+        results, _, _ = self.eval_model(self.args.num_train_epochs-1, global_step)
+        logging.info(results)
+        return global_step, tr_loss / global_step
+
+    def eval_model(self, epoch, global_step):
+        results = {}
+
+        eval_loss = 0.0
+        nb_eval_steps = 0
+        n_batches = len(self.test_dl)
+        # TODO: check the value of len(self.test_examples)
+        test_sample_len = len(self.test_examples)
+        preds = np.empty((test_sample_len, self.num_labels))
+
+        out_label_ids = np.empty(test_sample_len)
+        self.model.eval()
+        logging.info("len(test_dl) = %d, n_batches = %d" % (len(self.test_dl), n_batches))
+        for i, batch in enumerate(self.test_dl):
+            with torch.no_grad():
+                batch = tuple(t for t in batch)
+
+                sample_index_list = batch[0].to(self.device).cpu().numpy()
+                if i == len(self.test_dl) - 1:
+                    logging.info(batch)
+                x = batch[1].to(self.device)
+                labels = batch[4].to(self.device)
+
+                logits = self.model(x)
+
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+                eval_loss += loss.item()
+                # logging.info("test. batch index = %d, loss = %s" % (i, str(eval_loss)))
+
+            nb_eval_steps += 1
+            start_index = self.args.eval_batch_size * i
+
+            end_index = start_index + self.args.eval_batch_size if i != (n_batches - 1) else test_sample_len
+            logging.info("batch index = %d, start_index = %d, end_index = %d" % (i, start_index, end_index))
+            preds[start_index:end_index] = logits.detach().cpu().numpy()
+            out_label_ids[start_index:end_index] = labels.detach().cpu().numpy()
+
+        eval_loss = eval_loss / nb_eval_steps
+
+        model_outputs = preds
+        preds = np.argmax(preds, axis=1)
+        # logging.info("preds = " + str(preds))
+        # logging.info("out_label_ids = " + str(out_label_ids))
+        result, wrong = self.compute_metrics(preds, out_label_ids, self.test_examples)
+        result["eval_loss"] = eval_loss
+        results.update(result)
+
+        os.makedirs(self.args.output_dir, exist_ok=True)
+        output_eval_file = os.path.join(self.args.output_dir, "eval_results.txt")
+        with open(output_eval_file, "w") as writer:
+            for key in sorted(result.keys()):
+                writer.write("{} = {}\n".format(key, str(result[key])))
+        if result["acc"] > self.best_accuracy:
+            self.best_accuracy = result["acc"]
+        logging.info("best_accuracy = %f" % self.best_accuracy)
+        if self.args.global_rank == 0:
+            wandb.log({"Evaluation Accuracy (best)": self.best_accuracy, "step": global_step})
+            wandb.log({"Evaluation Accuracy": result["acc"], "step": global_step})
+            wandb.log({"Evaluation Loss": result["eval_loss"], "step": global_step})
+
+        self.results.update(result)
+        logging.info(self.results)
+
+        return result, model_outputs, wrong
+
+    def compute_metrics(self, preds, labels, eval_examples=None):
+        assert len(preds) == len(labels)
+
+        extra_metrics = {}
+        extra_metrics["acc"] = sklearn.metrics.accuracy_score(labels, preds)
+        mismatched = labels != preds
+
+        if eval_examples:
+            wrong = [i for (i, v) in zip(eval_examples, mismatched) if v.any()]
+        else:
+            wrong = ["NA"]
+
+        mcc = matthews_corrcoef(labels, preds)
+
+        tn, fp, fn, tp = confusion_matrix(labels, preds, labels=[0, 1]).ravel()
+        return (
+            {**{"mcc": mcc, "tp": tp, "tn": tn, "fp": fp, "fn": fn}, **extra_metrics},
+            wrong,
+        )
+
+    def build_optimizer(self, model, iteration_in_total):
+        warmup_steps = math.ceil(iteration_in_total * self.args.warmup_ratio)
+        self.args.warmup_steps = warmup_steps if self.args.warmup_steps == 0 else self.args.warmup_steps
+        logging.info("warmup steps = %d" % self.args.warmup_steps)
+        # optimizer = torch.optim.Adam(self._get_optimizer_grouped_parameters(), lr=self.args.learning_rate, betas=(0.9, 0.999), weight_decay=0.01)
+        optimizer = AdamW(model.parameters(), lr=self.args.learning_rate,
+                          eps=self.args.adam_epsilon)
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer, num_warmup_steps=self.args.warmup_steps, num_training_steps=iteration_in_total
+        )
+        return optimizer, scheduler
