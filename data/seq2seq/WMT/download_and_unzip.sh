c976ce11dbe0089dbb0fff6cfaa2c062f0acbccf
yuchenlin <yuchen.lin@usc.edu>
2021-04-01 14:28:28 -0700

    move the download scripts to a seperate folder

--- data/seq2seq/WMT/download_and_unzip.sh
+++
@@ -1,5 +0,0 @@
-rm training-parallel-nc-v13.tgz
-rm -rf training-parallel-nc-v13
-wget http://data.statmt.org/wmt18/translation-task/training-parallel-nc-v13.tgz
-tar -xvzf training-parallel-nc-v13.tgz
-rm training-parallel-nc-v13.tgz
