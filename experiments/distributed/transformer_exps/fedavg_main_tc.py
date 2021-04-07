9528a98e0b4618ad4210df1439b27cc050b77616
yuchenlin <yuchen.lin@usc.edu>
2021-04-07 02:57:15 -0700

    some todo items

--- experiments/distributed/transformer_exps/fedavg_main_tc.py
+++ experiments/distributed/transformer_exps/fedavg_main_tc.py
@@ -1,3 +1,11 @@
+from training.fed_trainer_transformer import FedTransformerTrainer
+from experiments.utils.general import set_seed, create_model, add_federated_args
+from data_preprocessing.text_classification_preprocessor import TLMPreprocessor
+from training.tc_transformer_trainer import TextClassificationTrainer
+from model.transformer.model_args import ClassificationArgs
+from data_manager.text_classification_data_manager import TextClassificationDataManager
+from FedML.fedml_api.distributed.utils.gpu_mapping import mapping_processes_to_gpu_device_from_yaml_file
+from FedML.fedml_api.distributed.fedavg.FedAvgAPI import FedML_init, FedML_FedAvg_distributed
 import argparse
 import logging
 import os
@@ -17,19 +25,6 @@ import wandb
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../")))
 
-from FedML.fedml_api.distributed.fedavg.FedAvgAPI import FedML_init, FedML_FedAvg_distributed
-from FedML.fedml_api.distributed.utils.gpu_mapping import mapping_processes_to_gpu_device_from_yaml_file
-
-from data_manager.text_classification_data_manager import TextClassificationDataManager
-from model.transformer.model_args import ClassificationArgs
-from training.tc_transformer_trainer import TextClassificationTrainer
-
-from data_preprocessing.text_classification_preprocessor import TLMPreprocessor
-
-from experiments.utils.general import set_seed, create_model, add_federated_args
-from training.fed_trainer_transformer import FedTransformerTrainer
-
-
 
 def create_model(args, num_labels):
     # create model, tokenizer, and model config (HuggingFace style)
@@ -37,9 +32,11 @@ def create_model(args, num_labels):
         "bert": (BertConfig, BertForSequenceClassification, BertTokenizer),
     }
     config_class, model_class, tokenizer_class = MODEL_CLASSES[model_type]
-    config = config_class.from_pretrained(model_name, num_labels=num_labels, **args.config)
+    config = config_class.from_pretrained(
+        model_name, num_labels=num_labels, **args.config)
     model = model_class.from_pretrained(model_name, config=config)
-    tokenizer = tokenizer_class.from_pretrained(model_name, do_lower_case=args.do_lower_case)
+    tokenizer = tokenizer_class.from_pretrained(
+        model_name, do_lower_case=args.do_lower_case)
     # logging.info(self.model)
     return config, model, tokenizer
 
@@ -54,66 +51,76 @@ def post_complete_message(tc_args):
         pipe.write("training is finished! \n%s" % (str(tc_args)))
 
 
-def load_data_to_FedML():
+def load_data_to_FedML(model_args, args, process_id, num_workers, preprocessor):
+    # TODO: process_id should be 0 if we want to load all instances
+    dm = TextClassificationDataManager(
+        model_args, args, process_id, num_workers, preprocessor) 
     dm.load_next_round_data()  # The centralized version.
     train_dl, test_dl = dm.get_data_loader()
     test_examples = dm.test_examples
-    return train_data_num, train_data_global, test_data_global, train_data_local_num_dict, train_data_local_dict, test_data_local_dict
+    # TODO:
+    return (train_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict)
+
 
 def fedavg_main(process_id, worker_number, device, args):
-    
-    # dataset attributes
-    attributes = TextClassificationDataManager.load_attributes(args.data_file_path)
 
+    # dataset attributes
+    attributes = TextClassificationDataManager.load_attributes(
+        args.data_file_path)
 
     # model init
-    model_args = ClassificationArgs()    
+    model_args = ClassificationArgs()
     model_args.model_name = args.model_name
     model_args.model_type = args.model_type
     model_args.load(model_args.model_name)
     model_args.num_labels = len(attributes["label_vocab"])
     model_args.update_from_dict({"num_train_epochs": args.num_train_epochs,
-                              "learning_rate": args.learning_rate,
-                              "gradient_accumulation_steps": args.gradient_accumulation_steps,
-                              "do_lower_case": args.do_lower_case,
-                              "manual_seed": args.manual_seed,
-                              "reprocess_input_data": True, # for ignoring the cache features.
-                              "overwrite_output_dir": True,
-                              "max_seq_length": args.max_seq_length,
-                              "train_batch_size": args.train_batch_size,
-                              "eval_batch_size": args.eval_batch_size,
-                              "evaluate_during_training_steps": args.evaluate_during_training_steps,
-                              "fp16": args.fp16,
-                              "data_file_path": args.data_file_path,
-                              "partition_file_path": args.partition_file_path,
-                              "partition_method": args.partition_method,
-                              "dataset": args.dataset,
-                              "output_dir": args.output_dir,
-                              "is_debug_mode": args.is_debug_mode
-                              })
+                                 "learning_rate": args.learning_rate,
+                                 "gradient_accumulation_steps": args.gradient_accumulation_steps,
+                                 "do_lower_case": args.do_lower_case,
+                                 "manual_seed": args.manual_seed,
+                                 # for ignoring the cache features.
+                                 "reprocess_input_data": True,
+                                 "overwrite_output_dir": True,
+                                 "max_seq_length": args.max_seq_length,
+                                 "train_batch_size": args.train_batch_size,
+                                 "eval_batch_size": args.eval_batch_size,
+                                 "evaluate_during_training_steps": args.evaluate_during_training_steps,
+                                 "fp16": args.fp16,
+                                 "data_file_path": args.data_file_path,
+                                 "partition_file_path": args.partition_file_path,
+                                 "partition_method": args.partition_method,
+                                 "dataset": args.dataset,
+                                 "output_dir": args.output_dir,
+                                 "is_debug_mode": args.is_debug_mode
+                                 })
 
-
-    model_config, client_model, tokenizer = create_model(model_args, formulation="classification")
+    model_config, client_model, tokenizer = create_model(
+        model_args, formulation="classification")
 
     # data preprocessor
-    preprocessor = TLMPreprocessor(args=model_args, label_vocab=attributes["label_vocab"], tokenizer=tokenizer)
+    preprocessor = TLMPreprocessor(
+        args=model_args, label_vocab=attributes["label_vocab"],
+        tokenizer=tokenizer)
 
     # data manager
     num_workers = 1
-    dm = TextClassificationDataManager(model_args, args, process_id, num_workers, preprocessor)
-    
- 
-    
-    client_trainer = TextClassificationTrainer(model_args, device, client_model, None, None, None)
-    fed_trainer = FedTransformerTrainer(client_trainer, client_model, task_formulation="classification")
 
-    train_data_num, train_data_global, test_data_global, train_data_local_num_dict, train_data_local_dict, test_data_local_dict = load_data_to_FedML(dm)
+    client_trainer = TextClassificationTrainer(
+        model_args, device, client_model, None, None, None)
+    fed_trainer = FedTransformerTrainer(
+        client_trainer, client_model, task_formulation="classification")
+
+    train_data_num, train_data_global, test_data_global, train_data_local_num_dict, \
+        train_data_local_dict, test_data_local_dict = load_data_to_FedML(
+            model_args, args, process_id, num_workers, preprocessor)
     # start FedAvg algorithm
     # for distributed algorithm, train_data_gloabl and test_data_global are required
-    FedML_FedAvg_distributed(process_id, worker_number, device, comm,
-                             client_model, train_data_num, train_data_global, test_data_global,
-                             train_data_local_num_dict, train_data_local_dict, test_data_local_dict, args,
-                             fed_trainer)
+    FedML_FedAvg_distributed(
+        process_id, worker_number, device, comm, client_model, train_data_num,
+        train_data_global, test_data_global, train_data_local_num_dict,
+        train_data_local_dict, test_data_local_dict, args, fed_trainer)
 
 
 if __name__ == "__main__":
@@ -124,9 +131,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # customize the log format
-    logging.basicConfig(level=logging.INFO,
-                        format='%(process)s %(asctime)s.%(msecs)03d - {%(module)s.py (%(lineno)d)} - %(funcName)s(): %(message)s',
-                        datefmt='%Y-%m-%d,%H:%M:%S')
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(process)s %(asctime)s.%(msecs)03d - {%(module)s.py (%(lineno)d)} - %(funcName)s(): %(message)s',
+        datefmt='%Y-%m-%d,%H:%M:%S')
     logging.info(args)
 
     set_seed(args.manual_seed)
@@ -149,12 +157,12 @@ if __name__ == "__main__":
             config=args)
 
     # device: check "gpu_mapping.yaml" to see how to define the topology
-    device = mapping_processes_to_gpu_device_from_yaml_file(process_id, worker_number, \
-                                                            args.gpu_mapping_file, args.gpu_mapping_key)
-    logging.info("process_id = %d, size = %d, device=%s" % (process_id, worker_number, str(device)))
+    device = mapping_processes_to_gpu_device_from_yaml_file(
+        process_id, worker_number, args.gpu_mapping_file, args.gpu_mapping_key)
+    logging.info("process_id = %d, size = %d, device=%s" %
+                 (process_id, worker_number, str(device)))
 
     fedavg_main(process_id, worker_number, device, args)
-
 
     # TODO: add the wandb later.
     # if args.global_rank == 0:
@@ -166,5 +174,3 @@ if __name__ == "__main__":
 
     if args.local_rank == 0:
         post_complete_message(args)
-
-    
