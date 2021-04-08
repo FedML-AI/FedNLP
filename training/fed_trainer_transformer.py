3c5fb2ab390538364fa14c8602df345244957349
yuchenlin <yuchen.lin@usc.edu>
2021-04-07 19:11:08 -0700

    set data

--- training/fed_trainer_transformer.py
+++ training/fed_trainer_transformer.py
@@ -31,14 +31,14 @@ class FedTransformerTrainer(ModelTrainer):
 
     def train(self, train_data, device, args):
         logging.info("Client(%d)"%self.id + ":| Local Train Data Size = %d" % (len(train_data)))
-        self.client_trainer.set_data(train_dl=train_data)
+        self.client_trainer.train_dl = train_data
         self.client_trainer.train_model()
 
 
 
     def test(self, test_data, device, args=None):
         logging.info("Client(%d)"%self.id + ":| Local Train Data Size = %d" % (len(test_data)))
-        self.client_trainer.set_data(test_dl=test_data)
+        self.client_trainertest_dl = test_data
         self.client_trainer.eval_model()
 
         # if self.task_formulation == "classification":
