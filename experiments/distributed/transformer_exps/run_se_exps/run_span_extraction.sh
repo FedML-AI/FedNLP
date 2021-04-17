224bd6f880a3905378aa9b2ee45d1e83a7b6621f
yuchenlin <yuchen.lin@usc.edu>
2021-04-17 01:09:13 -0700

    add se files

---
+++ experiments/distributed/transformer_exps/run_se_exps/run_span_extraction.sh
@@ -0,0 +1,48 @@
+FL_ALG=$1
+PARTITION_METHOD=$2
+C_LR=$3
+S_LR=$4
+ROUND=$5
+
+LOG_FILE="fedavg_transformer_se.log"
+WORKER_NUM=10
+CI=0
+
+DATA_DIR=~/fednlp_data/
+DATA_NAME=squad_1.1
+PROCESS_NUM=`expr $WORKER_NUM + 1`
+echo $PROCESS_NUM
+
+hostname > mpi_host_file
+
+mpirun -np $PROCESS_NUM -hostfile mpi_host_file \
+python -m fedavg_main_se \
+  --gpu_mapping_file "gpu_mapping.yaml" \
+  --gpu_mapping_key mapping_config2_11 \
+  --client_num_per_round $WORKER_NUM \
+  --comm_round $ROUND \
+  --ci $CI \
+  --dataset "${DATA_NAME}" \
+  --data_file "${DATA_DIR}/data_files/${DATA_NAME}_data.h5" \
+  --partition_file "${DATA_DIR}/partition_files/${DATA_NAME}_partition.h5" \
+  --partition_method $PARTITION_METHOD \
+  --fl_algorithm $FL_ALG \
+  --model_type distilbert \
+  --model_name distilbert-base-uncased \
+  --do_lower_case True \
+  --train_batch_size 8 \
+  --eval_batch_size 8 \
+  --max_seq_length 128 \
+  --lr $C_LR \
+  --server_lr $S_LR \
+  --epochs 1 \
+  --output_dir "/tmp/fedavg_${DATA_NAME}_output/" \
+  --fp16
+  2> ${LOG_FILE} &
+
+
+# sh run_span_extraction.sh FedAvg "niid_cluster_clients=10_alpha=5.0" 1e-5 0.1 50
+
+# sh run_span_extraction.sh FedProx "niid_cluster_clients=10_alpha=5.0" 1e-5 0.1 50
+
+# sh run_span_extraction.sh FedOPT "niid_cluster_clients=10_alpha=5.0" 1e-5 0.1 50
--- experiments/distributed/transformer_exps/run_text_classification.sh
+++ experiments/distributed/transformer_exps/run_text_classification.sh
@@ -19,7 +19,7 @@ hostname > mpi_host_file
 mpirun -np $PROCESS_NUM -hostfile mpi_host_file \
 python -m fedavg_main_tc \
   --gpu_mapping_file "gpu_mapping.yaml" \
-  --gpu_mapping_key mapping_config2_11 \
+  --gpu_mapping_key "mapping_ink-lucy" \
   --client_num_per_round $WORKER_NUM \
   --comm_round $ROUND \
   --ci $CI \
