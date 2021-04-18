aca174948e4e9c468a81cca46cfbc424bc101eb8
chaoyanghe <me@chaoyanghe.com>
2021-04-18 01:42:01 -0700

    debug

--- experiments/distributed/transformer_exps/sweep_tc.py
+++ experiments/distributed/transformer_exps/sweep_tc.py
@@ -60,15 +60,16 @@ os.system("kill $(ps aux | grep \"fedavg_main_tc.py\" | grep -v grep | awk '{pri
 hps = [
     # 'FedAvg "niid_label_clients=100_alpha=10.0" 1e-1 1 0.5 25',
     # 'FedAvg "niid_label_clients=100_alpha=1.0" 1e-1 1 0.5 25',
-    #'FedAvg "niid_label_clients=100_alpha=5.0" 1e-1 1 0.1 25', # lambda1
+    # 'FedAvg "niid_label_clients=100_alpha=5.0" 1e-1 1 0.1 25',
     # 'FedAvg "uniform" 1e-1 1 0.5 25',
     # 'FedAvg "niid_quantity_clients=100_beta=5.0" 1e-1 1 0.5 25',
 
-    #'FedProx "niid_label_clients=100_alpha=1.0" 1e-1 1 0.1 25', # lambda2
-    'FedProx "niid_label_clients=100_alpha=5.0" 1e-1 1 0.1 25',
-    'FedProx "niid_label_clients=100_alpha=10.0" 1e-1 1 0.1 25',
-    'FedProx "uniform" 1e-1 1 0.1 25',
-    'FedProx "niid_quantity_clients=100_beta=5.0" 1e-1 1 0.1 25',
+    #'FedProx "niid_label_clients=100_alpha=1.0" 1e-1 1 0.1 25',
+    #'FedProx "niid_label_clients=100_alpha=5.0" 1e-1 1 0.1 25',
+    'FedProx "niid_label_clients=100_alpha=10.0" 1e-1 1 0.1 25', # lambda1
+
+    # 'FedProx "uniform" 1e-1 1 0.1 25', # lambda3
+    # 'FedProx "niid_quantity_clients=100_beta=5.0" 1e-1 1 0.1 25', # Chaoyang
 
     # 'FedOPT "niid_label_clients=100_alpha=5.0" 5e-5 0.1 0.5 25',
     # 'FedOPT "niid_label_clients=100_alpha=10.0" 5e-5 0.1 0.5 25',
