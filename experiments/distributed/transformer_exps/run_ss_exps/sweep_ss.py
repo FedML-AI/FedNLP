3ed4776ef1f94b87fc91414211965d073177aa7b
Chaoyang He <chaoyanghe.com@gmail.com>
2021-05-16 13:14:10 -0700

    Update sweep_ss.py

--- experiments/distributed/transformer_exps/run_ss_exps/sweep_ss.py
+++ experiments/distributed/transformer_exps/run_ss_exps/sweep_ss.py
@@ -15,7 +15,8 @@ def add_args(parser):
 def wait_for_the_training_process():
     pipe_path = "./tmp/fedml"
     if not os.path.exists(pipe_path):
-        os.mknod(pipe_path)
+        with open(pipe_path, 'w'):
+            pass
     pipe_fd = os.open(pipe_path, os.O_RDONLY | os.O_NONBLOCK)
     with os.fdopen(pipe_fd) as pipe:
         while True:
