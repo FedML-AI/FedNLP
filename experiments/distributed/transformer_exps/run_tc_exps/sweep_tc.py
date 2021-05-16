cbbcdf1e63241d6f697aa62e4f2feaa378dd9c10
Chaoyang He <chaoyanghe.com@gmail.com>
2021-05-16 13:40:01 -0700

    Update sweep_tc.py

--- experiments/distributed/transformer_exps/run_tc_exps/sweep_tc.py
+++ experiments/distributed/transformer_exps/run_tc_exps/sweep_tc.py
@@ -86,8 +86,24 @@ hps = [
     'FedOPT "niid_label_clients=100_alpha=1.0" 5e-5 0.1 30 10 e',
 ]
 
+hps_ch = [
+    'FedOPT “niid_label_clients=100_alpha=0.1” 5e-5 1 100 10'
+    'FedOPT “niid_label_clients=100_alpha=0.5” 5e-5 1 100 10'
+    'FedOPT “niid_label_clients=100_alpha=1.0” 5e-5 1 100 10'
+    'FedOPT “niid_label_clients=100_alpha=5.0” 5e-5 1 100 10'
+    'FedOPT “niid_label_clients=100_alpha=10.0” 5e-5 1 100 10'
+    'FedOPT “niid_quantity_clients=100_beta=5.0” 5e-5 1 100 10'
+
+    'FedAvg “niid_label_clients=100_alpha=0.1” 1e-1 1 100 10'
+    'FedAvg “niid_label_clients=100_alpha=0.5” 1e-1 1 100 10'
+    'FedAvg “niid_label_clients=100_alpha=1.0” 1e-1 1 100 10'
+    'FedAvg “niid_label_clients=100_alpha=5.0” 1e-1 1 100 10'
+    'FedAvg “niid_label_clients=100_alpha=10.0” 1e-1 1 100 10'
+    'FedAvg “niid_quantity_clients=100_beta=5.0” 1e-1 1 100 10'
+]
+
 run_id = 0
-for hp in hps:
+for hp in hps_ch:
     args.hp = hp
     args.run_id = run_id
 
